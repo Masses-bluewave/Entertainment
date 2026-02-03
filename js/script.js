8c61e22adb64afa25b62f14dca0567d3e7335ff0
isaacicy <massesbluewave@gmail.com>
2026-02-03 12:20:22 +0200

    2

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ class AppHeader extends HTMLElement {
     <title>Masses</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-      <link rel="stylesheet" href="/css/style.css">
+      <link rel="stylesheet" href="./css/style.css">
        
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
