da50f2447ec8890abd04f63c4692fe91bd6b50b1
isaacicy <massesbluewave@gmail.com>
2026-02-03 11:51:33 +0200

    lego

---
+++ js/script.js
@@ -0,0 +1,53 @@
+class AppHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Masses</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+      <link rel="stylesheet" href="/css/style.css">
+       
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../index.html">Masses</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Events</a>
+        </li>
+       <li class="nav-item">
+          <a class="nav-link" href="#">Merch</a>
+        </li>
+      </ul>
+
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
+    </div>
+  </div>
+</nav>
+    `;
+  }
+}
+
+class AppFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <footer style="border:2px solid blue; padding:5px; margin-top: 20px;">
+        <p>&copy; 2026 My Website</p>
+      </footer>
+    `;
+  }
+}
+
+customElements.define('app-header', AppHeader);
+customElements.define('app-footer', AppFooter);
